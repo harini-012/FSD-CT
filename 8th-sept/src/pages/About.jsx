ea290fd59326483a324805f488125fb2ba6b1873
HARINI <harinirajesh0519@gmail.com>
2026-09-08 14:43:22 +0530

    Added new fies

---
+++ 8th-sept/src/pages/About.jsx
@@ -0,0 +1,21 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function About() {
+  return (
+    <>
+      <Navbar />
+
+      <h1>About Us</h1>
+
+      <p>
+        My College offers courses in Computer Science,
+        Artificial Intelligence and Data Science.
+      </p>
+
+      <Footer />
+    </>
+  );
+}
+
+export default About;
---
+++ 8th-sept/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <h1>Welcome to My College</h1>
+
+      <p>
+        We provide quality education in Computer Science.
+      </p>
+
+      <button>Learn More</button>
+
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
